07e6448f94ad4b1ec9136a9ad12ac83b178378bf
laurelinn <laurelinn@LAURELINNE8DD>
2016-06-02 12:23:13 -0600

    Initial shell projects

---
+++ AzureIoTHub/AzureIoTHub.CreateDevice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureIoTHub.CreateDevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
