38d5a753ff3dc42d0114972c1c00c09a0c30326a
laurelinn <laurelinn@LAURELINNE8DD>
2016-06-02 14:35:41 -0600

    Creates device, sends messages to IoT hub, display messages revieved.

--- AzureIoTHub/AzureIoTHub.CreateDevice/Program.cs
+++ AzureIoTHub/AzureIoTHub.CreateDevice/Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
 
 namespace AzureIoTHub.CreateDevice
 {
     class Program
     {
+
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=AzureIoTHubPOC.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=maxvsCzNfkLU+9s0/MeQp8ldRrI5cjTP3X5ATgHglHI=";
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Creating Device");
+
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            
+            Console.WriteLine("Device Created");
+            Console.ReadLine();
+
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "laureFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
         }
     }
 }
--- AzureIoTHub/AzureIoTHub.SimulatedDevice/Program.cs
+++ AzureIoTHub/AzureIoTHub.SimulatedDevice/Program.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
 
 namespace AzureIoTHub.SimulatedDevice
 {
     class Program
     {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "AzureIoTHubPOC.azure-devices.net";
+        static string deviceKey = "co8N4uugP8c1U3OAJqgbwCTqfZf9usoHMdwM11y9rmw=";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("linntestdevice", deviceKey));
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "linntestdevice",
+                    windSpeed = currentWindSpeed
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(1000).Wait();
+            }
         }
     }
 }
